53e702ffed8863eeefc9c2a49d83a9c1bbe0eb17
cohesionTech <hsoftdev11@gmail.com>
2021-10-08 09:12:57 -0400

    Demo spring_boot_microservices

---
+++ department-service/src/main/java/com/cohesionTech/department/repository/DepartmentRepository.java
@@ -0,0 +1,11 @@
+package com.cohesionTech.department.repository;
+
+import com.cohesionTech.department.entity.Department;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface DepartmentRepository extends JpaRepository<Department, Long> {
+
+    Department findByDepartmentId(Long departmentId);
+}
